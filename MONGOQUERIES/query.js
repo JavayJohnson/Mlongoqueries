542790efc24d427f0fa34e15c1aecfd6e2a601e5
javay johnson <javayjohnson@gmail.com>
2024-09-08 19:53:18 -0400

    first commit

---
+++ MONGOQUERIES/query.js
@@ -0,0 +1,32 @@
+Sample_airbnb: listingsAndReviews
+Filter data by property type
+{property_type : "House"}
+Query Results: 1-20 of many
+
+
+
+Sample_airbnb: listingsAndReviews
+Filter data by accomodations greater than 15 using the $gt operator
+{ accommodates: { $gt: 15 } }
+Query Results: 1-20 of many
+
+
+
+
+Sample_guides: Planets
+Filter data by Planetary order from sun less than 4 using the $lt operator
+{orderFromSun: {$lt: 4} }
+Query Results: 1-3 of 3
+
+
+
+
+
+Sample_airbnb: listingsAndReviews
+Filter data by Apartments or houses using the $in operator
+{ property_type: { $in: ["Apartment", "House"] } }
+Query Results: 1-20 of many
+
+
+
+
